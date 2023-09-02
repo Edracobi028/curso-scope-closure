30b405d0dc3e88c4e4d31482bac9e3d842b8af15
Eduardo Razo <esse_rios_e@hotmail.com>
2023-09-02 16:39:46 -0600

    Block Scope 4/13

---
+++ src/scope/block.js
@@ -0,0 +1,16 @@
+//Block Scope Esto se introdujo en ES6
+function fruits() {
+    //Definir un bloque es todo lo que esta dentro de llaves {} 
+
+    if (true) {
+        var fruit1 = 'Apple'; //function scope
+        let fruit2 = 'Kiwi'; // block scope
+        const fruit3 = 'Banana'; // block scope
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+
+    console.log(fruit1);
+}
+
+fruits();
