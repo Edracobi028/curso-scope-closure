059847492ce93cba891808d563b6df3bb0fc581d
DEVASM <erazo@agristar.com.mx>
2023-09-07 18:09:03 -0600

    Strict Mode 6/13

---
+++ src/scope/strict.js
@@ -0,0 +1,11 @@
+'use strict'; //Activar el modo estricto3
+pi = 3.1416;
+console.log(pi);
+
+//Tambien funciona en un alcance limitado como en una funcion
+function myfunction () {
+    'use strict';
+    return pi = 3.1416;
+}
+
+console.log(myFunction());
